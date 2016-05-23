3651b1fd4e5de0043cc45e61be04084f2ba7f74d
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2016-05-23 22:49:31 +0300

    fix saving after trimming

--- extension.ts
+++ extension.ts
@@ -1,13 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
   vscode.workspace.onDidSaveTextDocument(event => {
     vscode.commands.executeCommand('editor.action.trimTrailingWhitespace');
+    vscode.window.activeTextEditor.document.save();
   });
-}   
-
-
+}
